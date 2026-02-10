75b1426ff4726a3dbb840cc5fc884d7f8a0ce70a
Matheus <matheusnuma@gmail.com>
2026-02-09 22:03:49 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  
+  ///AAA - Arrange, Act, Assert
+  //Arrange
   await page.goto('http://localhost:5173/');
-
-  //checkpoint 1: deve estar na página de landing
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //checkpoint 2: deve estar na página de consulta de pedidos
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  
+  //Act
   await page.getByTestId('search-order-id').click();
   await page.getByTestId('search-order-id').fill('VLO-JKQ87Z');
 
-  //checkpoint 3: deve exibir o pedido aprovado
+  //Assert
   await page.getByTestId('search-order-button').click();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-JKQ87Z');
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+
 
 });
